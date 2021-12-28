23f754ac45d46c773a8a9a74807c5db212a78986
Shane McDonough <shanemcdman@gmail.com>
2021-12-27 22:10:57 -0800

    Make segments rainbow

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const SEG_LENGTH = 100;
-const NUMBER_OF_SEGS = 10; // must be >= 1
-let segments;
+const SEG_LENGTH = 10;
+const NUMBER_OF_SEGS = 200; // must be >= 1
+let COLORS;
+let segments = [];
+
+function get_color(){
+    if(typeof this.count === 'undefined')
+        this.count = 0;
+    return COLORS[this.count++ % COLORS.length];
+}
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    let prev = new Segment(
-        createVector(windowWidth / 2, windowHeight / 2),
-        SEG_LENGTH,
-        0,
-        color(255)
-    );
-    segments = [prev];
-    for(let i = 1; i < NUMBER_OF_SEGS; i++){
-        let seg = new Segment(
-            prev.end.copy(),
+    COLORS = [
+        color(255, 0, 0), // red
+        color(255, 165, 0), // orange
+        color(255, 255, 0), // yellow 
+        color(0, 128, 0), // green
+        color(0, 0, 255), // blue
+        color(75, 0, 130), // indigo
+        color(238, 130, 238), // violet
+    ];
+    for(let i = 1; i < NUMBER_OF_SEGS; i++)
+        segments.push(new Segment(
+            createVector(windowWidth / 2, windowHeight / 2),
             SEG_LENGTH,
             0,
-            color(255)
-        );
-        segments.push(seg);
-        prev = seg;
-    }
+            get_color()
+        ));
 }
 
 function draw(){
